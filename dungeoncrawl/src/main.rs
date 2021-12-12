036a23beb2975df78057e87a07db032200914e8a
xtopala <xtopala@gmail.com>
2021-12-12 13:22:36 +0100

    finished adding the adventurer

---
+++ dungeoncrawl/src/main.rs
@@ -0,0 +1,45 @@
+mod map;
+mod player;
+
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+}
+
+use prelude::*;
+
+use crate::player::Player;
+
+fn main() -> BError {
+    struct State {
+        map: Map,
+        player: Player,
+    }
+
+    impl State {
+        fn new() -> Self {
+            Self {
+                map: Map::new(),
+                player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)),
+            }
+        }
+    }
+
+    impl GameState for State {
+        fn tick(&mut self, ctx: &mut BTerm) {
+            ctx.cls();
+            self.player.update(ctx, &self.map);
+            self.map.render(ctx);
+            self.player.render(ctx);
+        }
+    }
+
+    let ctx = BTermBuilder::simple80x50()
+        .with_title("Dungeon Crawler")
+        .with_fps_cap(30.0)
+        .build()?;
+
+    main_loop(ctx, State::new())
+}
