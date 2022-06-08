003c38ab56cb97ca070be2417376d584899683b2
xtopala <xtopala@gmail.com>
2022-06-08 20:25:28 +0200

    finished Invertory and Powerups

--- dungeoncrawl/src/components.rs
+++ dungeoncrawl/src/components.rs
@@ -3,8 +3,8 @@ use std::collections::HashSet;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Render {
-    pub color : ColorPair,
-    pub glyph : FontCharType
+    pub color: ColorPair,
+    pub glyph: FontCharType,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -25,41 +25,57 @@ pub struct MovingRandomly;
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct ChasingPlayer;
 
-
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct WantsToMove {
-    pub entity : Entity,
-    pub destination : Point
+    pub entity: Entity,
+    pub destination: Point,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct WantsToAttack {
-    pub attacker : Entity,
-    pub victim : Entity
+    pub attacker: Entity,
+    pub victim: Entity,
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Health {
     pub current: i32,
-    pub max: i32
+    pub max: i32,
 }
 
 #[derive(Clone, PartialEq)]
 pub struct Name(pub String);
 
 #[derive(Clone, Debug, PartialEq)]
-pub struct FieldOfView{
-    pub visible_tiles : HashSet<Point>,
+pub struct FieldOfView {
+    pub visible_tiles: HashSet<Point>,
     pub radius: i32,
-    pub is_dirty: bool
+    pub is_dirty: bool,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct ProvidesHealing {
+    pub amount: i32,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct ProvidesDungeonMap;
+
+#[derive(Clone, PartialEq)]
+pub struct Carried(pub Entity);
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct ActivateItem {
+    pub used_by: Entity,
+    pub item: Entity,
 }
 
 impl FieldOfView {
     pub fn new(radius: i32) -> Self {
-        Self{
+        Self {
             visible_tiles: HashSet::new(),
             radius,
-            is_dirty: true
+            is_dirty: true,
         }
     }
 
